4796ad8bfec6d05a418e20fb0e96a7e06d1c9a1c
nurulalam <jack245200@gmail.com>
2022-08-21 23:24:19 +0600

    header and all file attach

---
+++ js/calculation.js
@@ -0,0 +1,34 @@
+document
+  .getElementById("btn-playerTotal-price")
+  .addEventListener("click", function () {
+    const getPerPlayerPrice = document.getElementById("per-player-price");
+    const perPlayerPriceString = getPerPlayerPrice.value;
+    const perPlayerPrice = parseFloat(perPlayerPriceString);
+
+    const totalPlayerPrice = perPlayerPrice * 5;
+
+    const getPreviousTotalPrice = document.getElementById("total-player-price");
+    const previousTotalPriceString = getPreviousTotalPrice.innerText;
+    const previousTotalPrice = parseFloat(previousTotalPriceString);
+
+    getPreviousTotalPrice.innerText = totalPlayerPrice + previousTotalPrice;
+  });
+
+document.getElementById("btn-fullCost").addEventListener("click", function () {
+  const getManagePrice = document.getElementById("manage-price");
+  const managePriceString = getManagePrice.value;
+  const managePrice = parseFloat(managePriceString);
+
+  const getCoachPrice = document.getElementById("coach-price");
+  const coachPriceString = getCoachPrice.value;
+  const coachPrice = parseFloat(coachPriceString);
+
+  const getPerPlayerPrice = document.getElementById("per-player-price");
+  const perPlayerPriceString = getPerPlayerPrice.value;
+  const perPlayerPrice = parseFloat(perPlayerPriceString);
+
+  const totalPlayerPrice = perPlayerPrice * 5;
+
+  const setTotalCost = document.getElementById("total-cost");
+  setTotalCost.innerText = totalPlayerPrice + coachPrice + managePrice;
+});
